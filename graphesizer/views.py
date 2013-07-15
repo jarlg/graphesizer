a8f07ffb04b1ef022c8c38325aaa0d8b6ef78f09
Jarl Gunnar T. Flaten <jarl.flaten@gmail.com>
2013-07-15 17:08:24 +0200

    better system for initial drawing of graph, and GET to /generate now redirects to /, / now gets signal and draws it initially; potential for easier sharing of functions

--- graphesizer/views.py
+++ graphesizer/views.py
@@ -1,19 +1,22 @@
 from graphesizer import app
 from forms import SignalForm
 from sound import SoundFile
-from flask import render_template, request, json, url_for
+from flask import render_template, request, json, url_for, redirect
 
 
 @app.route("/")
 @app.route("/<signal>")
-def index(signal=None):
+def index(signal="sin(10x)"):
     form = SignalForm()
     return render_template("index.html",
                            signal = signal,
                            form = form)
 
-@app.route("/generate", methods=["POST"])
+@app.route("/generate", methods=["GET", "POST"])
 def generate():
+    if request.method == "GET":
+        return redirect(url_for('index'))
+
     data = json.loads(request.data)
     w = SoundFile(audio=data['audio'])
     w.wav_from_audio()
