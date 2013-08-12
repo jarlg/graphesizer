4cda289b095ddfdf91ba417cca74494c8a34f6dc
Jarl Gunnar T. Flaten <jarl.flaten@gmail.com>
2013-08-12 16:53:19 +0200

    fix issue where perfect samples wouldn\'t sample

--- graphesizer/static/js/sampler.js
+++ graphesizer/static/js/sampler.js
@@ -7,7 +7,7 @@ function sample_audio(f, hz) {
 	var SAMPLES;
 	var audio = [];
 	var base_audio = [];
-	var DURATION = 1; // default dur; changed if we have a frequency to go by
+	var DURATION = 10; // default dur; changed if we have a frequency to go by
 	var RATE = 96000.0; // best results so far 
 
 	if (!checkbox.checked) { 
@@ -19,18 +19,23 @@ function sample_audio(f, hz) {
 			var diff = base_samples - Math.floor(base_samples);
 			var base_diff = diff;
 			var factor = 1;
-			while (true) {
-				if (diff < threshold) {
-					SAMPLES = Math.floor(base_samples * (factor - 1));
-					break;
-				}
-				else if (diff > (1 - threshold)) {
-					SAMPLES = Math.round(base_samples * (factor - 1));
-					break;
-				}
+			if (diff == 0) {
+				SAMPLES = Math.floor(base_samples)
+			}
+			else {
+				while (true) {
+					if (diff < threshold) {
+						SAMPLES = Math.floor(base_samples * (factor - 1));
+						break;
+					}
+					else if (diff > (1 - threshold)) {
+						SAMPLES = Math.round(base_samples * (factor - 1));
+						break;
+					}
 
-				diff = base_diff * factor - Math.floor(base_diff * factor);
-				factor++;
+					diff = base_diff * factor - Math.floor(base_diff * factor);
+					factor++;
+				}
 			}
 		}
 		else {
@@ -56,8 +61,24 @@ function sample_audio(f, hz) {
 	}
 
 	var TOT_SAMPLES = RATE * DURATION;
-	for (var i = 0; i < TOT_SAMPLES; i++) {
-		audio[i] = base_audio[i%base_audio.length];
+	if (base_audio.length < TOT_SAMPLES) {
+		for (var i = 0; i < TOT_SAMPLES; i++) {
+			audio[i] = base_audio[i%base_audio.length];
+		}
+	}
+	else {
+		audio = base_audio;
+	}
+
+	// fade ends of audio, to reduce jitter
+	var fade_threshold = 0.005 * RATE;
+	for (var i = 0; i < audio.length; i++) {
+		if (i < fade_threshold) {
+			audio[i] = audio[i] * i / fade_threshold;
+		}
+		else if (i > (audio.length - fade_threshold)) {
+			audio[i] = audio[i] * Math.abs(i - audio.length) / fade_threshold;
+		}
 	}
 
 	return audio;
